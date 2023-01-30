a518ecff525bb1ca90c0de8badcbbf459f11a63e
Shiva Rakshith <80666319+shiva-rakshith@users.noreply.github.com>
2023-01-30 14:51:08 +0530

    Update HCXApplication.java

--- hcx-apis/src/main/java/org/swasth/hcx/HCXApplication.java
+++ hcx-apis/src/main/java/org/swasth/hcx/HCXApplication.java
@@ -3,6 +3,7 @@ package org.swasth.hcx;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.swasth.hcx.controllers.HealthController;
 
 @EnableAspectJAutoProxy
 @SpringBootApplication
@@ -10,6 +11,8 @@ public class HCXApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HCXApplication.class, args);
+		HealthController healthController = new HealthController();
+		System.out.println(healthController.serviceHealth().getBody());
 	}
 
 }
