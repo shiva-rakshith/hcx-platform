8bf45c8748c65b224bb8d9988994a8cdbc00a822
Shiva Rakshith <80666319+shiva-rakshith@users.noreply.github.com>
2023-01-30 15:11:10 +0530

    Update HCXApplication.java

--- hcx-apis/src/main/java/org/swasth/hcx/HCXApplication.java
+++ hcx-apis/src/main/java/org/swasth/hcx/HCXApplication.java
@@ -1,18 +1,22 @@
 package org.swasth.hcx;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.swasth.common.utils.JSONUtils;
 import org.swasth.hcx.controllers.HealthController;
+
+import java.util.Map;
 
 @EnableAspectJAutoProxy
 @SpringBootApplication
 public class HCXApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws JsonProcessingException {
 		SpringApplication.run(HCXApplication.class, args);
 		HealthController healthController = new HealthController();
-		System.out.println(healthController.serviceHealth().getBody());
+		System.out.println(JSONUtils.serialize(healthController.serviceHealth().getBody()));
 	}
 
 }
